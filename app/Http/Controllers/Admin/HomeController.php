7073ddd167af3f4354ea4409f1b73da20d65c75a
Nicola-Perera <nicola.perera96@mail.com>
2022-01-27 15:57:14 +0100

    fix bug with database migration

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -14,6 +15,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        ddd(Auth::user());
         return view('admin.home');
     }
 }
